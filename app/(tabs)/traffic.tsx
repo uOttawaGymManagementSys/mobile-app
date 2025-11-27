25aa470e02ec73a77dba068882731fc7c3699502
Oussema Nijewi <oussemasn@gmail.com>
2025-11-27 00:06:49 -0500

    traffic count static UI

--- app/(tabs)/traffic.tsx
+++ app/(tabs)/traffic.tsx
@@ -2,16 +2,30 @@ import { useGym } from "@/context/GymContext";
 import React from "react";
 import { Text, View } from "react-native";
 
-const traffic = () => {
+export default function Traffic() {
   const { selectedGymId } = useGym();
+
+  const gymName =
+    selectedGymId === 4
+      ? "Minto"
+      : selectedGymId === 3
+      ? "Montpetit"
+      : "No Gym Selected";
+
   return (
     <View className="flex-1 bg-primary px-6 pt-12">
+      {/* Title */}
       <Text className="text-white text-3xl font-bold mt-8">Latest Traffic</Text>
-      <Text className="text-white/80 mt-4 font-semibold">
-        Selected gym ID: {selectedGymId ?? "please select a gym to see results"}
-      </Text>
+
+      {/* Center Circle */}
+      <View className="flex-1 justify-center items-center mt-[-150px]">
+        <View className="w-60 h-60 rounded-full border-4 border-gray-600 justify-center items-center">
+          <Text className="text-white text-7xl font-bold">42</Text>
+          <Text className="text-white/80 text-xl font-semibold mt-2">
+            {gymName}
+          </Text>
+        </View>
+      </View>
     </View>
   );
-};
-
-export default traffic;
+}
