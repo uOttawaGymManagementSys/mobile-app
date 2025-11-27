f035b1215487b520e67e188a5176814433b46d0b
Oussema Nijewi <oussemasn@gmail.com>
2025-11-26 23:24:13 -0500

    home screen static UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,38 @@
-import { View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
 export default function Index() {
-  return <View className="flex-1 bg-primary"></View>;
+  return (
+    <View className="flex-1 bg-primary px-6 pt-12">
+      <Text className="text-white text-3xl font-bold mt-8">Gym Insights</Text>
+      <Text className="text-white/80 mt-4 font-semibold">Select a gym</Text>
+
+      <View className="mt-20 gap-4">
+        {/* Minto */}
+        <Pressable className="rounded-2xl bg-white/15 px-5 py-4 ml-5 mr-5 active:bg-gray-100">
+          {({ pressed }) => (
+            <Text
+              className={`text-lg font-semibold text-center ${
+                pressed ? "text-black" : "text-white"
+              }`}
+            >
+              Minto
+            </Text>
+          )}
+        </Pressable>
+
+        {/* Montpetit */}
+        <Pressable className="rounded-2xl bg-white/15 px-5 py-4 ml-5 mr-5 active:bg-gray-100">
+          {({ pressed }) => (
+            <Text
+              className={`text-lg font-semibold text-center ${
+                pressed ? "text-black" : "text-white"
+              }`}
+            >
+              Montpetit
+            </Text>
+          )}
+        </Pressable>
+      </View>
+    </View>
+  );
 }
